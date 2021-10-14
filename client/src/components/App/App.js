1c205317026cd351a725ca1c79f5d2036c6870db
TheMiksa <michael.gritskov@gmail.com>
2021-10-15 00:02:36 +0300

    Added socket to the App

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import './App.css';
+import socket from 'apiRequests/socket';
 
 function App() {
+
+  useEffect(() => {
+      console.log('here!');
+
+      socket.on('ticker', data => {
+          console.log('data: ', data);
+      });
+      socket.emit('start');
+  }, []);
   return (
     <div className="App">
-      app
+      appj
     </div>
   );
 }
