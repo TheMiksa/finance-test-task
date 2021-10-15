2e7a9317ab4cec8cf3489518d4a1c2bb04991b7b
TheMiksa <michael.gritskov@gmail.com>
2021-10-15 22:21:00 +0300

    Added transformToCamelCase hepler

---
+++ client/src/helpers/transformToCamelCase.js
@@ -0,0 +1,15 @@
+const transformToCamelCase = data => (
+    data.map(oldObject => {
+        const newObject = {};
+
+        for (const key in oldObject) {
+            const newKey = key.split('_')
+                .map((word, index) => index > 0 ? word[0].toUpperCase() + word.slice(1) : word).join('');
+
+            newObject[newKey] = oldObject[key];
+        }
+
+        return newObject;
+    }));
+
+export default transformToCamelCase;
