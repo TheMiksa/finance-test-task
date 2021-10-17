5afe2ed38f91ee36f4a4734cd58f30c9c8791635
TheMiksa <michael.gritskov@gmail.com>
2021-10-17 18:23:31 +0300

    Implemented TickerHistory component

--- client/src/components/common/TickerHistory/TickerHistory.js
+++ client/src/components/common/TickerHistory/TickerHistory.js
@@ -1,7 +1,17 @@
 import './TickerHistory.css';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 const TickerHistory = ({ data }) => {
-    return <div>TickerHistory</div>
+    const newData = data.map((el, index) => ({ name: index, uv: +el.change }));
+    return <div>
+        <LineChart width={800} height={400} data={newData}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" name="$" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+        </LineChart>
+    </div>
 };
 
 export default TickerHistory;
