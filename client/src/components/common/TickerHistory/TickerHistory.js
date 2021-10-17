56ab5f75f2cba07cc0f7106014fd74425246ec34
TheMiksa <michael.gritskov@gmail.com>
2021-10-17 19:04:47 +0300

    Small refactoring

--- client/src/components/common/TickerHistory/TickerHistory.js
+++ client/src/components/common/TickerHistory/TickerHistory.js
@@ -2,7 +2,7 @@ import './TickerHistory.css';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 const TickerHistory = ({ data }) => {
-    const newData = data.map((el, index) => ({ name: '', change: +el.change }));
+    const newData = data.map(element => ({ name: '', change: +element.change }));
     return <div>
         <LineChart width={800} height={400} data={newData}>
             <Line type="monotone" dataKey="change" stroke="#8884d8" name="change" />
