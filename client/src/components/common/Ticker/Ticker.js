2d10dfef139eb8a7bc001a05db816d57ca9c0e85
TheMiksa <michael.gritskov@gmail.com>
2021-10-17 16:14:48 +0300

    Added logic to display TickerHistory

--- client/src/components/common/Ticker/Ticker.js
+++ client/src/components/common/Ticker/Ticker.js
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import './Ticker.css';
 import { testId } from 'constants/testingConstants';
+import { ReactComponent as PlusIcon} from 'assets/icons/plus.svg';
+import { ReactComponent as MinusIcon} from 'assets/icons/minus.svg';
 import { getChange, getChangePercent } from 'helpers';
+import { Button, TickerHistory } from 'components';
 
 const Ticker = ({ data }) => {
+   const [isHistoryOpen, setIsHistoryOpen] = useState(false);
+
    return (
-       <div className="tickerBlock" data-testid={testId.ticker}>
-          <div className="tickerName">{data.ticker}</div>
-          <div className="tickerCompany">{data.company}</div>
-          <div className="tickerPrice">{data.price}</div>
-          {getChange(data.change)}
-          {getChangePercent(data.changePercent)}
+       <div className="tickerWrapper">
+           <div className="tickerBlock" data-testid={testId.ticker}>
+               <div className="tickerName">{data.ticker}</div>
+               <div className="tickerCompany">{data.company}</div>
+               <div className="tickerPrice">{data.price}</div>
+               {getChange(data.change)}
+               {getChangePercent(data.changePercent)}
+               {isHistoryOpen ? (
+                   <Button onClick={() => setIsHistoryOpen(false)}>
+                       <MinusIcon />
+                   </Button>
+               ) : (
+                   <Button onClick={() => setIsHistoryOpen(true)}>
+                       <PlusIcon />
+                   </Button>
+               )}
+           </div>
+           {isHistoryOpen && <TickerHistory data={data.changeHistory} />}
        </div>
    );
 };
